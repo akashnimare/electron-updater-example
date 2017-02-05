6921b66769a8f6c886c217b4b9d52705cfd31ccd
Akash Nimare <akashnimare@users.noreply.github.com>
2017-02-06 01:57:55 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 
 const {app, BrowserWindow, Menu, protocol, ipcMain} = require('electron');
 const log = require('electron-log');
-const {autoUpdater} = require("electron-updater");
+const {autoUpdater} = require("electron-updater").autoUpdater;
 
 //-------------------------------------------------------------------
 // Logging
